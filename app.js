f119571009e4378e351eafcee7387e86772d3a68
gvipul-294 <gvipul294@gmail.com>
2018-02-27 06:28:26 +0530

    server setup;

---
+++ app.js
@@ -0,0 +1,17 @@
+var express=require('express');
+var app=express();
+var server =require('http').createServer(app);
+var io=require('socket.io').listen(server);
+users= [];
+connections= [];
+
+server.listen(3000);
+console.log('Server is running!');
+
+_dirname='C:/Pro/Chat app/Chat-Application-using-NodeJs-';
+
+app.get('/',function (req,res) {
+	// body...
+	res.sendFile(_dirname+'/index.html');
+
+})
