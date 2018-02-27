826c8f66894c4e1681df1785a39ac8d59ad6d51e
gvipul-294 <gvipul294@gmail.com>
2018-02-27 07:24:18 +0530

    Socket function declared

--- app.js
+++ app.js
@@ -13,5 +13,12 @@ _dirname='C:/Pro/Chat app/Chat-Application-using-NodeJs-';
 app.get('/',function (req,res) {
 	// body...
 	res.sendFile(_dirname+'/index.html');
+});
 
-})
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('State_Connect: %s sockets connected', connections.length);
+	//Disconnect
+	connections.splice(connections.indexOf(socket),1);
+	console.log('State_Disconnect: %s sockets connected', connections.length);
+});
